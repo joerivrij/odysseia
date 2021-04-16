39b6ad6146f801a868823df47d2455ef07230381
joerivrij <joerivrijaldenhoven@gmail.com>
2021-04-16 17:51:33 +0200

    feat: swagger added

--- sokrates/pkg/impl/routes.go
+++ sokrates/pkg/impl/routes.go
@@ -12,7 +12,7 @@ func InitRoutes(config config.SokratesConfig) *mux.Router {
 
 	sokratesHandler := SokratesHandler{Config: &config}
 
-	serveMux.HandleFunc("/ping", middleware.Adapt(sokratesHandler.PingPong, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
+	serveMux.HandleFunc("api/v1/ping", middleware.Adapt(sokratesHandler.PingPong, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
 	serveMux.HandleFunc("/api/v1/health", middleware.Adapt(sokratesHandler.PingPong, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
 	serveMux.HandleFunc("/api/v1/chapters/{category}", middleware.Adapt(sokratesHandler.FindHighestChapter, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
 	serveMux.HandleFunc("/api/v1/createQuestion", middleware.Adapt(sokratesHandler.CreateQuestion, middleware.ValidateRestMethod("GET"), middleware.LogRequestDetails()))
